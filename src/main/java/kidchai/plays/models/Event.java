22ef2edc3dcd627453cd72d2a5704082a44c1990
Kidchai <o.naibova@gmail.com>
2022-10-07 12:43:43 -0500

    Implement Event and WebScraper classes

---
+++ src/main/java/kidchai/plays/models/Event.java
@@ -0,0 +1,76 @@
+package kidchai.plays.models;
+
+public class Event {
+    private String imageURL;
+    private String title;
+    private String theatreName;
+    private String genres;
+    private String earliestDate;
+    private String latestDate;
+    private String description;
+    private String details;
+
+    public String getEarliestDate() {
+        return earliestDate;
+    }
+
+    public void setEarliestDate(String earliestDate) {
+        this.earliestDate = earliestDate;
+    }
+
+    public String getLatestDate() {
+        return latestDate;
+    }
+
+    public void setLatestDate(String latestDate) {
+        this.latestDate = latestDate;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getGenres() {
+        return genres;
+    }
+
+    public void setGenres(String genres) {
+        this.genres = genres;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTheatreName() {
+        return theatreName;
+    }
+
+    public void setTheatreName(String theatreName) {
+        this.theatreName = theatreName;
+    }
+}
---
+++ src/main/java/kidchai/plays/webscraper/WebScraper.java
@@ -0,0 +1,67 @@
+package kidchai.plays.webscraper;
+
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.*;
+import kidchai.plays.models.Event;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WebScraper {
+
+    public static void main(String[] args) {
+        var client = new WebClient();
+        client.getOptions().setCssEnabled(false);
+        client.getOptions().setJavaScriptEnabled(false);
+
+        var searchUrl = "https://chicagoplays.com/find-a-show/?vemsearch%5Blisting%5D=1366&vemsearch%5Bkeyword%5D=&vemsearch%5Bstart%5D=10%2F10%2F2022&vemsearch%5Bend%5D=10%2F27%2F2022&vemsearch%5Bgenre%5D=0&vemsearch%5Bneighborhood%5D=0&vemsearch%5Btheatre%5D=0&p1366=2";
+        List<Event> eventList = new ArrayList<>();
+
+        try {
+            HtmlPage page = client.getPage(searchUrl);
+            var eventsNodeList = page.querySelectorAll(".vem-single-event");
+
+            for (var eventNode : eventsNodeList) {
+                Event event = new Event();
+
+                var imageURLParent = eventNode.querySelector(".vem-single-event-thumbnail > a");
+                DomElement imageURL = (DomElement) imageURLParent.getFirstChild();
+                var link = imageURL.getAttribute("src");
+
+                event.setImageURL(link);
+                System.out.printf("url: %s\n", link);
+
+                var title = eventNode.querySelector(".vem-single-event-title");
+                event.setTitle(title.getTextContent());
+
+
+                var theatreName = eventNode.querySelector(".vem-single-event-theatre-name");
+                event.setTheatreName(theatreName.getTextContent());
+
+                var genres = eventNode.querySelector(".vem-single-event-genres");
+                event.setGenres(genres.getTextContent());
+
+                var dates = eventNode.querySelector(".vem-single-event-run-dates > span");
+                var earliestDate = dates.querySelector(".vem-earliest");
+                var latestDate = dates.querySelector(".vem-latest");
+                event.setEarliestDate(earliestDate.getTextContent());
+                event.setLatestDate(latestDate.getTextContent());
+
+                var description = eventNode.querySelector(".vem-single-event-excerpt");
+                event.setDescription(description.getTextContent());
+
+                var detailsParent = eventNode.querySelector(".vem-single-event-thumbnail");
+                DomElement detailsDomElement = (DomElement) detailsParent.getFirstChild();
+                var details = detailsDomElement.getAttribute("href");
+                event.setDetails(details);
+
+                eventList.add(event);
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("n");
+    }
+}
