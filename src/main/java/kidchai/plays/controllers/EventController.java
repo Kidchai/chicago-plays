b6a07bc51b1b2724e645c1a464df85891c783af2
Kidchai <o.naibova@gmail.com>
2023-07-18 21:48:09 -0500

    Remove code duplication from EventController

--- src/main/java/kidchai/plays/controllers/EventController.java
+++ src/main/java/kidchai/plays/controllers/EventController.java
@@ -1,7 +1,6 @@
 package kidchai.plays.controllers;
 
 import kidchai.plays.dtos.FilterDto;
-import kidchai.plays.model.Event;
 import kidchai.plays.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,8 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/events")
@@ -31,7 +28,6 @@ public class EventController {
 
     @GetMapping("")
     public String getAllEvents(@ModelAttribute FilterDto filter, Model model) {
-        List<Event> events = eventService.getAllEvents(filter);
         model.addAttribute("events", eventService.getAllEvents(filter));
         model.addAttribute("filter", filter);
         return "index";
