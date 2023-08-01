b38096e4f96702040051a111265a734eff8cf89d
Kidchai <o.naibova@gmail.com>
2023-08-01 10:09:21 -0500

    Format lines in Event.vue

--- client/src/components/Event.vue
+++ client/src/components/Event.vue
@@ -3,31 +3,23 @@
     class="flex max-w-xl flex-col items-start justify-between"
   >
     <div class="flex items-center gap-x-4 text-xs">
-      <time
-        :datetime="event.firstDate"
-        class="text-gray-500"
-      >{{ event.firstDate }}</time>
-      -
-      <time
-        :datetime="event.lastDate"
-        class="text-gray-500"
-      >{{ event.lastDate }}</time>
-      <a
+      <span class="text-gray-500">
+        <time :datetime="event.firstDate">{{ event.firstDate }}</time>
+        -
+        <time :datetime="event.lastDate">{{ event.lastDate }}</time>
+      </span>
+      <a v-for="genre in event.genres" :key="genre.id"
         class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
-      >{{ event.genres }}</a>
+      >{{ genre.genre }}</a>
 
-      <a v-if="event.minPrice"
-      class="text-gray-500"
-      >from ${{ event.minPrice }}
-    </a>
-    <a v-if="event.maxPrice"
-      class="text-gray-500"
-      >to ${{ event.maxPrice }}
-    </a>
+      <span v-if="event.minPrice || event.maxPrice" class="text-gray-500">
+        <template v-if="event.minPrice">from ${{ event.minPrice }}</template>
+        <template v-if="event.maxPrice"> to ${{ event.maxPrice }} </template>
+      </span>
     </div>
     <div class="group relative">
       <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-        <a :href="event.url">
+        <a :href="event.eventUrl" target="_blank">
           <span class="absolute inset-0" />
           {{ event.title }}
         </a>
