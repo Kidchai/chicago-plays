d532095da2d97599b82650743de720fc6df608e2
Kidchai <o.naibova@gmail.com>
2023-11-17 19:30:38 -0600

    add ingress networks

--- eks/setup.sh
+++ eks/setup.sh
@@ -8,13 +8,13 @@ aws ecr create-repository --repository-name chicago-plays-frontend --region $REG
 aws ecr create-repository --repository-name chicago-plays-backend  --region $REGION
 
 aws rds create-db-instance \
-    --db-instance-identifier chicago-plays-db \
-    --db-instance-class db.t3.micro \
-    --engine postgres \
-    --allocated-storage 20 \
-    --master-username postgres \
-    --master-user-password $DB_PASSWORD \
-    --no-publicly-accessible
+  --db-instance-identifier chicago-plays-db \
+  --db-instance-class db.t3.micro \
+  --engine postgres \
+  --allocated-storage 20 \
+  --master-username postgres \
+  --master-user-password $DB_PASSWORD \
+  --no-publicly-accessible
 
 eksctl create cluster --name chicago-plays --region $REGION --nodegroup-name standard-workers --node-type t2.medium --nodes 3 --nodes-min 1 --nodes-max 4 --managed
 
@@ -22,7 +22,9 @@ aws eks --region $REGION update-kubeconfig --name chicago-plays
 
 kubectl create secret generic chicago-plays-db-secret --from-literal=SPRING_DATASOURCE_PASSWORD=$DB_PASSWORD
 
-kubectl apply -f frontend-service.yaml,frontend-deployment.yaml,backend-service.yaml,backend-deployment.yaml
+for file in *.yaml; do
+  kubectl apply -f $file
+done
 
 # Status:
 # kubectl get deployments
